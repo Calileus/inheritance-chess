975ec6a1d658d0c201162cc98c80467d1327f732
Calileus <79671075+Calileus@users.noreply.github.com>
2026-01-27 23:08:15 -0600

    adding common.hpp file

---
+++ include/common.hpp
@@ -0,0 +1,25 @@
+/// ---------------------------------------------------------------------------------
+/// @file      common.hpp
+/// @author    Calileus (https://github.com/Calileus/inheritance-chess)
+/// @brief     Basic Chess Engine demonstrating C++ inheritance and polymorphism.
+/// @version   1.0
+/// @date      2026-01-27
+/// @copyright MIT License - see LICENSE file for details
+/// ---------------------------------------------------------------------------------
+#ifndef ICHESS_SRC_COMMON
+#define ICHESS_SRC_COMMON
+
+enum class PieceColor : char
+{
+  NONE  = ' ',
+  WHITE = 'w',
+  BLACK = 'b'
+};
+
+struct PiecePosition
+{
+    char file; // e.g., 'a' through 'h'
+    char rank; // e.g., '1' through '8'
+};
+
+#endif // ICHESS_SRC_COMMON
