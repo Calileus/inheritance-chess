6ebe9ccc399ddfb7c42d9f1c7f820c8d9d29b3cd
Calileus <79671075+Calileus@users.noreply.github.com>
2026-02-10 01:41:30 -0600

    Creating tests/test_board.cpp file with 3 tests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -22,7 +22,7 @@
 int main(int argc, char** argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
-  
+
   std::cout << "Running tests executed from main() defined in test_main.cpp...\n" << std::endl;
   int result = RUN_ALL_TESTS();
   if (result == 0)
